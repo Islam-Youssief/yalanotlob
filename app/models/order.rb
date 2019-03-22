1bd445021d7c2e2e462f36ffd181fcb1c4d43577
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-03-22 20:45:23 +0200

    Added Validation to Order Model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,11 +2,14 @@ class Order
   include Mongoid::Document
   field :meal, type: String
   field :restaurantName, type: String
-  field :menuImage, type: String
+  field :menuImage, type: Hash
   field :status, type: String
-  field :date, type: String
-  field :totalPrice, type: String
-  field :friends, type: String
-  field :groups, type: String
-  field :items, type: String
+  field :date, type: DateTime, default: Time.now
+  field :totalPrice, type: Integer, default: 0
+  field :friends, type: Array
+  field :groups, type: Array
+  field :items, type: Array
+  embeds_many :friends, store_as: "friend"
+  embeds_many :groups, store_as: "groups"
+  include Mongoid::Timestamps
 end
